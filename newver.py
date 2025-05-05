0a62e27cc1dee339dcd09dd7d733dbd7101014a4
Sergei <thegraycanal13542@gmail.com>
2025-05-05 11:09:12 +0300

    Added comments

--- newver.py
+++ newver.py
@@ -7,9 +7,12 @@ import plotly.graph_objects as go
 
 data = pd.read_csv('public_data.csv')
 data['date'] = pd.to_datetime(data['date'])
+
+# ставим начальный индекс на 1
 data.index = range(1,len(data)+1)
 final_dfs = []
-# print(data)
+
+#функция вывода графика
 def plot_country_volumes(df: pd.DataFrame, y: str):
     fig = px.line(df, x='date', y=y, labels={'date': 'Date'})
     fig.update_layout(template="simple_white", font=dict(size=18), title_text='CO2',
@@ -18,12 +21,17 @@ def plot_country_volumes(df: pd.DataFrame, y: str):
     return fig.show()
 
 
-
+# Перебираем каждую колонку, кроме даты
 for i in data.columns[1:]:
     model = SARIMAX(data[i][:-30], order=(1, 1, 1), seasonal_order=(1, 1, 1, 12))
     model_fit = model.fit()
+
+    # Вывод подробной информации
     # print(model_fit.summary())
+
+    # Вывод гарфиков стран без прогноза
     # plot_country_volumes(data, i)
+
     # Прогноз на основе обученной модели
     forecast = model_fit.forecast(steps=30)
 
@@ -34,25 +42,35 @@ for i in data.columns[1:]:
     print(f'MSE: {mse}')
     print(f'MAE: {mae}')
 
+    # Делаем прогноз на 31 день
     forecast_future = model_fit.forecast(steps=31)
 
 
     # Создаем новый DataFrame для будущих значений
     future_dates = pd.date_range(start='2023-05-01', periods=31, freq='D')
     forecast_df = pd.DataFrame({'date': future_dates, i: forecast_future})
+
+    # Присоединяем прогноз к списку с прогнозами
+    final_dfs.append(forecast_df)
+
+    # Вывод датафрейма с прогнозированными значениями
     # print(forecast_df)
 
-
-    # Присоединяем прогноз к исходному DataFrame
-    final_dfs.append(forecast_df)
-    print(forecast_df)
-    # plot_country_volumes(new_df, i)
 # print(final_dfs)
 res = final_dfs[0]
+
+# Присоединяем датафреймы с прогнозами к исходеым данным
 for i in range(len(data.columns)-2):
     combined_df = pd.merge(res, final_dfs[i+1], on = 'date')
     res = combined_df
+# Выставляем начальный индекс прогнозов на 1583
 res = res.reset_index(drop=True)
 res.index += 1583
 result = pd.concat([data, res])
+
+#вывод графиков с прогнозами выброса CO2 каждой страны
+# for i in data.columns[1:]:
+#     plot_country_volumes(result, i)
+
+#Создание итогового csv файла
 result.to_csv('output.csv', index=False)
